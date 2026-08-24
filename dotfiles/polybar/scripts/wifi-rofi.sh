cf7075015b9a747075486a2198653040f102b149
shahmeer-raza <shahmeerrazaarain@gmail.com>
2026-08-24 22:02:17 +0500

    fix: add password prompt for wifi connection via rofi

--- dotfiles/polybar/scripts/wifi-rofi.sh
+++ dotfiles/polybar/scripts/wifi-rofi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Get currently connected SSID
 current_ssid=$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d: -f2)
 
 if [[ "$1" == "--status" ]]; then
@@ -12,24 +11,21 @@ if [[ "$1" == "--status" ]]; then
     exit 0
 fi
 
-# List available Wi-Fi networks
 networks=$(nmcli -t -f SSID dev wifi list | grep -v '^$' | sort -u)
 
-# Add disconnect option if connected
 if [[ -n "$current_ssid" ]]; then
     networks="-- Disconnect --\n$networks"
 fi
 
-# Show selection menu
 chosen=$(echo -e "$networks" | rofi -dmenu -p "Select Wi-Fi:")
 
-if [[ -z "$chosen" ]]; then
-    exit 0
-fi
+[[ -z "$chosen" ]] && exit 0
 
 if [[ "$chosen" == "-- Disconnect --" ]]; then
     nmcli connection down "$current_ssid"
 else
-    nmcli device wifi connect "$chosen"
+    password=$(rofi -dmenu -p "Password for $chosen:" -password)
+    if [[ -n "$password" ]]; then
+        nmcli device wifi connect "$chosen" password "$password"
+    fi
 fi
-
