309b0a8bcec2d9200c95fe9adfbe7da6275c2adf
shahmeer-raza <shahmeerrazaarain@gmail.com>
2026-09-04 22:13:56 +0500

    style: change mute icon color to grey

--- dotfiles/polybar/scripts/pulseaudio-check.sh
+++ dotfiles/polybar/scripts/pulseaudio-check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 COLOR="#e49a6b"
+COLOR2="#566178"
 # Get mute status
 mute_status=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 
 # Check if muted
 if [[ "$mute_status" == "yes" ]]; then
-    echo "%{F$COLOR}%{F-}"
+    echo "%{F$COLOR2}%{F-}"
     exit 0
 fi
 
